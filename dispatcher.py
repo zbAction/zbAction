967cee6eaf6f0affe4a42e757e3de38194678faa
Andrew Lee <andrew.dragon.lee@gmail.com>
2015-07-26 22:29:01 -0400

    fixed small dispatcher bug

--- dispatcher.py
+++ dispatcher.py
@@ -13,15 +13,24 @@ class Dispatcher(Thread):
 				action = action_queue.pop(0)
 				receiver = action.receiver
 
-				del action.receiver
-
 				if receiver.toKey() in connections:
+					# Delete receiver so we don't send
+					# the receiver to the receiver.
+					# Will be restored before storing.
+					del action.receiver
+
 					connections[receiver.toKey()].write_message(
 						json.dumps(action.__dict__, default=serialize)
 					)
 
 					action.seen = True
-				
-				store_queue.append(action)
+				else:
+					action_queue.append(action)
+
+				# Note to self: need to check fo row
+				# with the exact same columns to avoid
+				# duplicates (do this inside store.py)
+				setattr(action, 'receiver', receiver)
+				store_queue.append(action)				
 
 			mutex.release()
