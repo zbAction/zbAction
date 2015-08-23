fdb2430396541fecdff4c0bf022a24a0f92d954e
Andrew Lee <andrew.dragon.lee@gmail.com>
2015-08-23 18:31:40 -0400

    More things, more standardized spacing

--- static/js/manager.js
+++ static/js/manager.js
@@ -7,8 +7,8 @@ $(function(){
 
         $.post(update_mod_keys_ep, {
             key: api_key
-        }).done(function(e){
-            if(e.status === 0){
+        }).done(function(resp){
+            if(resp.status === 0){
                 $container.remove();
             }
             else{
@@ -25,25 +25,57 @@ $(function(){
         var key = $('#new-mod-key').val().trim();
 
         if(mods.indexOf(key) === -1){
+            $('#add-new-panel button .fa-spinner').show();
+            $('#add-new-panel button .content').hide();
+
             $.post(get_mod_info_ep, {
                 key: key
             }).done(function(resp){
                 if(resp.status === 0 && resp.enabled){
                     $('#mod-name').text(resp.name || 'N/A');
                     $('#mod-key').text(resp.key);
+                    $('#add-mod-status').text('');
 
                     $('#add-new-panel').slideUp();
                     $('#confirm-panel').slideDown();
                 }
                 else{
-                    alert('This modification does not exist.');
+                    $('#add-mod-status').text('This modification does not exist.');
                 }
             }).fail(function(){
                 location.assign('/error/500')
+            }).always(function(){
+                $('#add-new-panel button .fa-spinner').hide();
+                $('#add-new-panel button .content').show();
             });
         }
         else{
-            alert('This modification is already in use.');
+            $('#add-mod-status').text('This modification is already in use.');
         }
-    })
+    });
+
+    $('#cancel-add').click(function(){
+        $('#add-new-panel').slideDown();
+        $('#confirm-panel').slideUp();
+    });
+
+    $('#confirm-add').click(function(){
+        var key = $('#new-mod-key').val().trim();
+
+        $('#confirm-add .fa-spinner').show();
+        $('#confirm-add .content').hide();
+
+        $.post(update_mod_keys_ep, {
+            key: key
+        }).done(function(resp){
+            if(resp.status === 0){
+                location.assign('/manager/');
+            }
+            else{
+                location.assign('/error/500');
+            }
+        }).fail(function(){
+            location.assign('/error/500');
+        });
+    });
 });
