9726004f0b1c3b002e84ab5eb28fc223070a0e0c
Andrew Lee <andrew.dragon.lee@gmail.com>
2015-08-24 00:05:41 -0400

    sql profiling

--- modules/manager.py
+++ modules/manager.py
@@ -1,32 +1,10 @@
 from flask import Blueprint, render_template, request
 from flask.ext.login import current_user, login_required
-from sqlalchemy.sql.functions import concat, count
+from sqlalchemy.sql.functions import count
 
 from db import session_factory
 from models.action import Action
 from models.mod import Mod
-
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-import time
-import logging
-
-logging.basicConfig()
-logger = logging.getLogger("myapp.sqltime")
-logger.setLevel(logging.DEBUG)
-
-@event.listens_for(Engine, "before_cursor_execute")
-def before_cursor_execute(conn, cursor, statement,
-                        parameters, context, executemany):
-    conn.info.setdefault('query_start_time', []).append(time.time())
-    logger.debug("Start Query: %s", statement)
-
-@event.listens_for(Engine, "after_cursor_execute")
-def after_cursor_execute(conn, cursor, statement,
-                        parameters, context, executemany):
-    total = time.time() - conn.info['query_start_time'].pop(-1)
-    logger.debug("Query Complete!")
-    logger.debug("Total Time: %f", total)
 
 manager = Blueprint('manager', __name__)
 
