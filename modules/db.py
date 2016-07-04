2d3d5d082ae394cc9addf10e9fd7fe3498a96f9f
Andrew <andrew.dragon.lee@gmail.com>
2016-07-04 15:28:34 -0400

    add announcement dashboard to main site

--- modules/db.py
+++ modules/db.py
@@ -25,3 +25,24 @@ def session_factory():
 		raise
 	finally:
 		s.close()
+
+f_eng = 'mysql://{username}:{password}@{host}:3307/{name}?charset=utf8'.format(**secrets.flarum_db.__dict__)
+f_engine = create_engine(
+	f_eng, pool_recycle=600
+)
+
+f_factory = sessionmaker(bind=f_engine)
+f_Session = scoped_session(f_factory)
+
+@contextmanager
+def flarum_session_factory():
+	s = f_Session()
+
+	try:
+		yield s
+		s.commit()
+	except:
+		s.rollback()
+		raise
+	finally:
+		s.close()
--- modules/routes.py
+++ modules/routes.py
@@ -1,11 +1,15 @@
 import os
 
-from flask import abort, redirect, render_template, send_file
+from flask import abort, redirect, render_template, send_file, url_for
+from flask.ext.login import current_user
 from jinja2.exceptions import TemplateNotFound
+import markdown
+from xml.etree import ElementTree
 
 from main import app, cache
 
-from db import session_factory
+from db import flarum_session_factory
+from object import Object
 
 # Routes
 
@@ -38,10 +42,100 @@ def docs(category, page):
 def support():
     return redirect('https://forum.zbaction.reticent.io')
 
+@app.route('/announcements')
+@app.route('/announcements/<int:id>')
+def announcements(id=None):
+    with flarum_session_factory() as sess:
+        if id is None:
+            with flarum_session_factory() as sess:
+                res = sess.execute(
+                    '''
+                    SELECT
+                        x.id,
+                        x.title,
+                        u.username AS 'author',
+                        x.start_time AS 'timestamp',
+                        z.content
+                    FROM flarum_discussions x
+                    JOIN 
+                        flarum_discussions_tags y
+                        ON x.id=y.discussion_id
+                    JOIN
+                        flarum_posts z
+                        ON x.start_post_id=z.id
+                    JOIN
+                        flarum_users u
+                        ON x.start_user_id=u.id
+                    WHERE
+                        y.tag_id=10
+                    ORDER BY x.start_time DESC
+                    '''
+                )
+
+                posts = [
+                    Object({
+                        'id': r.id,
+                        'title': r.title,
+                        'author': r.author,
+                        'timestamp': r.timestamp,
+                        'content': markdown.markdown(
+                            ''.join(ElementTree.fromstring(r.content).itertext()),
+                            extensions=['pymdownx.github']
+                        )
+                    }) for r in res
+                ]
+
+                return render_template('announcements.html', posts=posts)
+        else:
+            try:
+                res = sess.execute(
+                    '''
+                    SELECT
+                        x.id,
+                        x.title,
+                        u.username AS 'author',
+                        x.start_time AS 'timestamp',
+                        z.content
+                    FROM flarum_discussions x
+                    JOIN 
+                        flarum_discussions_tags y
+                        ON x.id=y.discussion_id
+                    JOIN
+                        flarum_posts z
+                        ON x.start_post_id=z.id
+                    JOIN
+                        flarum_users u
+                        ON x.start_user_id=u.id
+                    WHERE
+                        y.tag_id=10
+                        AND
+                        x.id={}
+                    ORDER BY x.start_time DESC
+                    '''.format(id)
+                )
+
+                post = [Object({
+                    'id': r.id,
+                    'title': r.title,
+                    'author': r.author,
+                    'timestamp': r.timestamp,
+                    'content': markdown.markdown(
+                        ''.join(ElementTree.fromstring(r.content).itertext()),
+                        extensions=['pymdownx.github']
+                    )
+                }) for r in res][0]
+
+                return render_template('post.html', post=post)
+            except:
+                return redirect(url_for('oops', err=404))
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        return announcements()
+    else:
+        return render_template('home.html')
 
 @app.route('/<int:uid>')
 def test(uid):
